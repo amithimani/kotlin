d81b48576eeab0f6c797af11edb79c4851b5ca9c
amithimani <amit.himani@outlook.com>
2021-02-24 21:36:06 +0000

    first commit

---
+++ src/stringtemplate/Condition.java
@@ -0,0 +1,11 @@
+package stringtemplate;
+
+public class Condition {
+    public static void main(String[] args) {
+        int x =101;
+        if (x % 2 == 0)
+            System.out.println("The number " + x +" is even.");
+        else
+            System.out.println("The number " + x +" is odd.");
+    }
+}
---
+++ src/stringtemplate/OddEvenCondition.kt
@@ -0,0 +1,8 @@
+package stringtemplate
+
+fun main(args: Array<String>) {
+    val carName = "BMW 5 Series"
+    val carModel = "Saloon 3.0"
+    val carPrice = 90000
+    println("$carName  car of model $carModel cost up to $carPrice Euros")
+}
